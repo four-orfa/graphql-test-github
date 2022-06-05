1c74da3235c9758aee30d9248df3f94957339260
Toshinori Sumida <alias.ceuta@gmail.com>
2022-06-06 02:58:59 +0900

    refactoring

--- src/App.js
+++ src/App.js
@@ -4,33 +4,9 @@ import { ApolloProvider } from '@apollo/client'
 import SearchRepositories from './SearchRepositories'
 
 const App = () => {
-  const [query, setQuery] = useState('')
-  const [state, setState] = useState({
-    first: 3,
-    after: null,
-    last: null,
-    before: null,
-    query: '',
-  })
-
-  const searchRepositories = () => {
-    setState({ ...state, query: query })
-  }
-
   return (
     <ApolloProvider client={client}>
-      <form>
-        <input
-          value={query}
-          onChange={(e) => {
-            setQuery(e.target.value)
-          }}
-        />
-        <button type='button' onClick={searchRepositories}>
-          Search
-        </button>
-      </form>
-      <SearchRepositories variables={state} />
+      <SearchRepositories />
     </ApolloProvider>
   )
 }
--- src/SearchRepositories.js
+++ src/SearchRepositories.js
@@ -1,6 +1,26 @@
+import { useState } from 'react'
 import { gql, useQuery } from '@apollo/client'
 
 const SearchRepositories = (props) => {
+  const PER_PAGE = 3
+
+  const [query, setQuery] = useState('')
+  const [state, setState] = useState({
+    first: PER_PAGE,
+    after: null,
+    last: null,
+    before: null,
+    query: '',
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  const searchRepositories = () => {
+    setState({ ...state, query: query })
+  }
+
   const { loading, error, data } = useQuery(
     gql`
       query searchRepositories($first: Int, $after: String, $last: Int, $before: String, $query: String!) {
@@ -30,7 +50,7 @@ const SearchRepositories = (props) => {
       }
     `,
     {
-      variables: { ...props.variables },
+      variables: { ...state },
     },
   )
 
@@ -40,19 +60,37 @@ const SearchRepositories = (props) => {
   const repositoryCount = data.search.repositoryCount
   const repositoryUnit = repositoryCount === 1 ? 'Repository' : 'Repositories'
   const title = `Github Repositories Search - ${repositoryCount} ${repositoryUnit}`
+
+  const nextPage = (search) => {}
+
   return (
     <>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={query}
+          onChange={(e) => {
+            setQuery(e.target.value)
+          }}
+        />
+        <button type='button' onClick={searchRepositories}>
+          Search
+        </button>
+      </form>
+
       <h2>{title}</h2>
       <ul>
         {data.search.edges.map((edge) => {
           const node = edge.node
           return (
             <li key={node.id}>
-              <a href={node.url}>{node.name}</a>
+              <a href={node.url} target='_blank' rel='noopener noreferrer'>
+                {node.name}
+              </a>
             </li>
           )
         })}
       </ul>
+      {data.search.pageInfo.hasNextPage ? <button onClick={nextPage(data.search)}>Next</button> : null}
     </>
   )
 }
