06e8406daffc60a58fef155afca4dd0a9e657713
Toshinori Sumida <alias.ceuta@gmail.com>
2022-06-08 01:02:45 +0900

    star button

--- src/StarButton.js
+++ src/StarButton.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 const StarButton = (props) => {
   const starCount = props.node.stargazers.totalCount
+  const viewerHasStarred = props.node.viewerHasStarred
   const title = starCount === 1 ? `1 star` : `${starCount} stars`
-  return <button>{title}</button>
+  const isStarred = viewerHasStarred ? 'starred' : '-'
+  return (
+    <>
+      <button>{title + ' | ' + isStarred}</button>
+    </>
+  )
 }
 
 export default StarButton
